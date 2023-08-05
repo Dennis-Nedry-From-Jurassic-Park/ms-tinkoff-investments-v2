99df314e9f00bd462bf86bd75dc21b0b5ac5d16d
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-08-05 16:06:59 +0300

    api

--- packages/backend/apps/api/src/api.ts
+++ packages/backend/apps/api/src/api.ts
@@ -1,4 +1,6 @@
-import {TinkoffInvestApi} from "tinkoff-invest-api/cjs/api";
+import {TinkoffInvestApi} from "tinkoff-invest-api";
+// https://github.com/vitalets/tinkoff-invest-api/issues/1
+// https://github.com/alpacahq/alpaca-ts/issues/106
 
 export const api = new TinkoffInvestApi({ token: process.env.TOKEN + '' });
 
