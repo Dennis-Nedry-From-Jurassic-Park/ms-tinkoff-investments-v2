535a976ad634e3110f130f74275667adf186d0de
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-08-21 14:03:09 +0300

    rm number.ts move to helpers/*

--- packages/backend/apps/api/src/helpers/num.ts
+++ packages/backend/apps/api/src/helpers/num.ts
@@ -1,3 +1,11 @@
+import BigNumber from "bignumber.js";
+import {MoneyValue, Quotation} from "tinkoff-invest-api/cjs/generated/common";
+import {api} from "../api";
+
+export const toBigNum = (value: Quotation | MoneyValue | undefined): BigNumber | undefined => {
+    return (value ? BigNumber(value.units + value.nano / 1000000000) : value) as BigNumber extends undefined ? undefined : BigNumber
+}
+
 const nanoPrecision = 1_000_000_000;
 
 export const toNum
@@ -7,6 +15,13 @@ export const toQuotation = (number: number) => ({
     units: Math.floor(number),
     nano: Math.trunc(number),
 });
+
+export const toNum_ = (value: Quotation | MoneyValue | undefined): number | undefined => {
+    return api.helpers.toNumber(value) ;
+}
+export const toQuotation_ = (value: number): Quotation => {
+    return api.helpers.toQuotation(value) ;
+}
 
 export const roundToNearestStep = (candidate, step) => {
     return Math.round(candidate / step) * step;
